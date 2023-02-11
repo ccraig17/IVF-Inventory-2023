32a062885b15fae41dc3a61cfdce1d87e2a55c6c
Colin Craig <73488952+ccraig17@users.noreply.github.com>
2023-02-11 14:23:01 -0500

    Update Menu.java
    Changed the showMenu() Method; unfortunately. it prints out the changes in the Console, but does NOT update the Database.  Not sure.

--- Menu.java
+++ Menu.java
@@ -11,24 +11,9 @@ public class Menu {
         menu.showMenu();
 
 
-
             menu.scanner.close();
         }
 
-//        private static Plastic authenticatePlastic (Plastic name){
-//            Plastic plastic = Datasource.getPlastic(name);
-//            if (plastic == null) {
-//                System.out.println("This item does NOT exist in the Plastics Inventory");
-//            }
-//            return plastic;
-//        }
-//        private static Media authenticateMedia(Media name){
-//            Media media = Datasource.getMedia(name);
-//            if (media == null) {
-//                System.out.println("This item does NOT exist in the Media Inventory");
-//            }
-//            return media;
-//        }
         private void showMenu (){
             int selection = 0;
 
@@ -45,33 +30,106 @@ public class Menu {
                 System.out.println("=============================================");
 
                 selection = scanner.nextInt();
-                int amount;
-                String name = null;
+                Media media = null;
+                String name;
+                int amount = 0;
+                int referenceNumber;
+                String lotNumber;
+                int currentBalance;
 
                 switch (selection) {
                     case 1: //adding plastic
-                        System.out.println("what plastic would you like to add?");
+                           // addPlastics(name, amount); //this method is supposed to take in the name and amount of item to add
+                        System.out.println("What is the name of the plastic you would like to add?");
                         name = scanner.next();
-                        System.out.println("how much would you like to add?");
+                        System.out.println("Please enter the reference number.");
+                        referenceNumber = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("please enter the lot number");
+                        lotNumber = scanner.next();
+                        System.out.println("Please enter the current balance.");
+                        currentBalance = scanner.nextInt();
+                        System.out.println("How much would you like like to add?");
                         amount = scanner.nextInt();
-                        //addPlastics(name, amount); //this method is supposed to take in the name and amount of item to add
+                        Plastic plastic = null;
+                        plastic = new Plastic(name, referenceNumber, lotNumber, currentBalance);
+                        try{
+                             plastic.addPlastics(name, amount);
+
+                        }catch(AmountException e){
+                            e.getMessage();
+                            System.out.println("Please try again");
+                        }
                         break;
-                    case 2: //remove Plastic
+                    case 2:
+                        System.out.println("What is the name of the plastic you would like to remove?");
+                        name = scanner.next();
+                        System.out.println("Please enter the reference number.");
+                        referenceNumber = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("please enter the lot number");
+                        lotNumber = scanner.next();
+                        System.out.println("Please enter the current balance.");
+                        currentBalance = scanner.nextInt();
+                        System.out.println("How much would you like like to remove?");
+                        amount = scanner.nextInt();
+                        plastic = new Plastic(name, referenceNumber, lotNumber, currentBalance);
+                        try{
+                            plastic.removePlastics(name, amount);
+                        }catch(AmountException e){
+                            e.getMessage();
+                            System.out.println("Please try again");
+                        }
                         break;
-                    case 3: //show plastic inventory
+                    case 3:
+                        Datasource.printPlasticInventory();
                         break;
                     case 4: //adding Media
-                        System.out.println("what Media would you like to add?");
-                        name = scanner.next();
-                        System.out.println("how much would you like to add?");
-                        amount = scanner.nextInt();
                         //addMedia(name, amount);
+                        System.out.println("What is the name of the Media you would like to add?");
+                        name = scanner.next();
+                        System.out.println("Please enter the reference number.");
+                        referenceNumber = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("please enter the lot number");
+                        lotNumber = scanner.next();
+                        System.out.println("Please enter the current balance.");
+                        currentBalance = scanner.nextInt();
+                        System.out.println("How much would you like like to add?");
+                        amount = scanner.nextInt();
+                        media = new Media(name, referenceNumber, lotNumber, currentBalance);
+                        try{
+                            media.addMedia(name, amount);
+                        }catch(AmountException e){
+                            e.getMessage();
+                            System.out.println("Please try again");
+                        }
                         break;
                     case 5: //remove Media
+                        System.out.println("What is the name of the media you would like to remove?");
+                        name = scanner.next();
+                        System.out.println("Please enter the reference number.");
+                        referenceNumber = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("please enter the lot number");
+                        lotNumber = scanner.next();
+                        System.out.println("Please enter the current balance.");
+                        currentBalance = scanner.nextInt();
+                        System.out.println("How much would you like like to remove?");
+                        amount = scanner.nextInt();
+                        media = new Media(name, referenceNumber, lotNumber, currentBalance);
+                        try{
+                            media.removeMedia(name, amount);
+                        }catch(AmountException e){
+                            e.getMessage();
+                            System.out.println("Please try again");
+                        }
                         break;
-                    case 6: //show Media inventory
+                    case 6:
+                        Datasource.printMediaInventory();
                         break;
-                    case 7: //exit menu
+                    case 7:
+                        System.out.println("Thank you for using 'The IVF INVENTORY!'");
                         break;
                     default:
                         System.out.println("Invalid selection; please selection between: 1-7.");
@@ -79,39 +137,7 @@ public class Menu {
                 }
             }
         }
-//        private void showMediaMenu(Media name){
-//            int selection = 0;
-//
-//            while (selection != 4) {
-//                System.out.println("===========================================");
-//                System.out.println("Please select from the following options: "
-//                        + "\n 1: Add a Media to Inventory"
-//                        + "\n 2: Remove a Media from Inventory"
-//                        + "\n 3: Show Media Inventory"
-//                        + "\n 4. Exit Menu");
-//                System.out.println("=============================================");
-//
-//                selection = scanner.nextInt();
-//                int amount;
-//
-//                switch (selection) {
-//                    case 1: //adding Media
-//                        System.out.println("what Media would you like to add?");
-//                        name = scanner.next();
-//                        System.out.println("how much would you like to add?");
-//                        amount = scanner.nextInt();
-//                        break;
-//                    case 2: //remove Media
-//                        break;
-//                    case 3: //show Media inventory
-//                        break;
-//                    case 4: //exit menu
-//                        break;
-//                    default:
-//                        System.out.println("Invalid selection; please selection between: 1-4.");
-//                        break;
-//                }
-//            }
-//        }
+
+
     }
 
